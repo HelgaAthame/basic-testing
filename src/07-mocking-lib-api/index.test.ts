50c0aa052265f8912fd01b561dba8dae6bf82eb7
HelgaAthame <athamethedarkest@gmail.com>
2023-06-29 16:07:13 +0300

    feat: pass almost all the tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,58 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+const testURL = '/posts/1';
+const data = {
+  someField: 'someValue',
+  otherField: 12345,
+  thirdField: true,
+};
+
+jest.mock('axios', () => {
+  return {
+    create: jest.fn(),
+  };
+});
+
+/*jest.mock('lodash/throttle', () => ({
+  default: jest.fn(fn => fn),
+  __esModule: true
+}))*/
+
+//axios.create({ baseURL: baseUrl });
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const createSpy = jest.spyOn(axios, 'create');
+    /*(axios.create as jest.MockedFunction<typeof axios.get>).mockResolvedValue(data);
+    await throttledGetDataFromApi(testURL);
+    expect(axios.get).toHaveBeenCalledWith(testURL);*/
+    //jest.runAllTimers();
+    console.dir(createSpy.mock);
+    expect(createSpy.mock.calls).toEqual(1);
+    expect(createSpy.mock.results[0]?.value.defaults).toBe(data);
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const createSpy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(testURL);
+    jest.runAllTimers();
+    console.log(createSpy.mock);
+    expect(createSpy.mock.calls).toEqual(1);
+    expect(createSpy.mock.results[0]?.value.defaults).toBe(data);
+// check for createSpy.mock.calls and createSpy.mock.results[0]?.value.defaults;
+    /*axios.get = jest.fn().mockResolvedValue(data);
+    await throttledGetDataFromApi(testURL);
+    expect(axios.get).toHaveBeenCalledWith(testURL);*/
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const createSpy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(testURL);
+    jest.runAllTimers();
+    console.log(createSpy.mock);
+    expect(createSpy.mock.calls).toEqual(1);
+    expect(createSpy.mock.results[0]?.value.defaults).toBe(data);
   });
 });
